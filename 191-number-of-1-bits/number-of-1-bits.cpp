4e28d6b5e1b49cc3b0a71b25e0dd6a93049a8166
Suthakar Anburaj <136780013+suthakaranburaj@users.noreply.github.com>
2026-06-08 21:20:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.92%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans = 0;
+
+        while(n != 0){
+            if(n%2 == 0){
+                n= n/2;
+            }
+            else{
+                n=n/2;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
